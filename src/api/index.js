19844712d11748d10ae9b43b7f7b76196948c2c8
mukesh786yadav <1032190391@tcetmumbai.in>
2024-03-10 21:32:59 +0530

    removed vercel file

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://memories-app-backend-delta.vercel.app/' });
+const API = axios.create({ baseURL: 'https://memories-app-backend-w12p.onrender.com' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
